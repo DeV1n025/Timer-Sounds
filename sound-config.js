83feead696d5f548e1db1d3459b4344872dba74e
DeV1n025 <vinci.seminara@icloud.com>
2026-04-30 23:04:21 +0200

    Add files via upload

---
+++ sound-config.js
@@ -0,0 +1,12 @@
+window.SOUND_CONFIG = {
+    alarmSounds: [
+        { label: 'Alarm 10', path: 'alarm.mp3' },
+        { label: 'Alarm 277', path: 'liftmu 2.mp3' },
+        { label: 'Alarm 676767', path: 'mukke 2.mp3' }
+    ],
+    musicSounds: [
+        { label: 'Muiisik 1', path: 'Liftmu1.mp3' },
+        { label: 'Musghhjrtzik 2', path: 'mukke 1.mp3' },
+        { label: 'Musigggk 3', path: 'alarm.mp3' }
+    ]
+};
